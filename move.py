cc5c29d9446e49ebab9542013ee97f148795a7bd
Liibert Paul <oliibert@hotmail.com>
2019-11-02 18:40:17 +0200

    move

--- move.py
+++ move.py
@@ -42,7 +42,7 @@ def on_press(key):
     elif k == 'q':
         if throw:
             ser.write("sd:-10:3:0\nd:1500\n".encode())
-        ser.write("sd:-5:2:0\n".encode())
+        ser.write("sd:-10:3:0\n".encode())
     elif k == 'd':
         if throw:
             throw = False
