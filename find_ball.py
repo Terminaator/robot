0b38b4b9603bc1fc7bca4399febd9ae924f0ca89
Liibert Paul <oliibert@hotmail.com>
2019-11-04 20:24:12 +0200

    ball

--- find_ball.py
+++ find_ball.py
@@ -81,8 +81,6 @@ while True:
             simg2 = cv2.rectangle(frame, (x, y), (x + w, y + h), 255, 2)
             centre_x = x + ((w) / 2)
             centre_y = y + ((h) / 2)
-            zDepth = depth_frame.get_distance(3,2)
-            print(zDepth)
             cv2.circle(frame, (int(centre_x), int(centre_y)), 3, (0, 110, 255), -1)
         cv2.imshow('Processed', frame)
         cv2.imshow('treshold', ball)
