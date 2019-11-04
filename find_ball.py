8d1cf3211c74cdbbca733c4ae0a688de052dcdb9
Liibert Paul <oliibert@hotmail.com>
2019-11-04 20:38:02 +0200

    ball

--- find_ball.py
+++ find_ball.py
@@ -19,6 +19,7 @@ cv2.createTrackbar("6", "Trackbars", 255, 255, nothing)
 pipeline = rs.pipeline()
 config = rs.config()
 
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 60)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 60)
 
 profile = pipeline.start(config)
