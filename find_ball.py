a030f66fc63f5a053178d6deaae13a17f335611e
Liibert Paul <oliibert@hotmail.com>
2019-11-11 18:15:32 +0200

    test

--- find_ball.py
+++ find_ball.py
@@ -19,8 +19,8 @@ cv2.createTrackbar("6", "Trackbars", 255, 255, nothing)
 pipeline = rs.pipeline()
 config = rs.config()
 
-config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.rgb8, 30)
 
 profile = pipeline.start(config)
 
@@ -67,7 +67,7 @@ while True:
     frame = np.asanyarray(color_frame.get_data())
 
     cv2.imshow('Processed', segment_colour(frame))
-    cv2.imshow('Processed', frame)
+    cv2.imshow('Processed1', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
