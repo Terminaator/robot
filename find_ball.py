fe16c1ff788c5e1b3d7df19553031fd13ae0fc36
Liibert Paul <oliibert@hotmail.com>
2019-11-11 18:07:02 +0200

    test

--- find_ball.py
+++ find_ball.py
@@ -20,7 +20,7 @@ pipeline = rs.pipeline()
 config = rs.config()
 
 config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 1280, 720, rs.format.rgb8, 30)
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
 
 profile = pipeline.start(config)
 
