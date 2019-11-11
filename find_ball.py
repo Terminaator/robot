9daf488ce7111eeb75df2b4c78da1de02fc7d410
Liibert Paul <oliibert@hotmail.com>
2019-11-11 18:51:17 +0200

    test

--- find_ball.py
+++ find_ball.py
@@ -31,9 +31,9 @@ def segment_colour(frame):  # returns only the red colors in the frame
                                         cv2.getTrackbarPos("3", "Trackbars")]),
                          np.array([cv2.getTrackbarPos("4", "Trackbars"), cv2.getTrackbarPos("5", "Trackbars"),
                                    cv2.getTrackbarPos("6", "Trackbars")]))
-    kernel = np.ones((3, 3), np.uint8)
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    return opening
+    #kernel = np.ones((3, 3), np.uint8)
+    #opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    return mask
 
 
 def find_blob(blob):  # returns the red colored circle
