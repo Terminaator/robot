5fec2e3cfd6fbea20458a38f196f2f417c3ede5e
Liibert Paul <oliibert@hotmail.com>
2019-11-18 15:25:13 +0200

    t

--- paul/vision.py
+++ paul/vision.py
@@ -63,4 +63,4 @@ class Vision(Thread):
         frame = cv2.warpAffine(frame_wrong_way, cv2.getRotationMatrix2D((320, 240), 90, 1), (640, 480))
         ball_mask = self.ball_mask(frame)
         ball_x,ball_y = self.find_ball(ball_mask)
-        self.AI.send_message("")
+        AI.send_message("")
