ef6f47c1b876888804244ffa634e42fb1c707569
Liibert Paul <oliibert@hotmail.com>
2019-11-18 15:04:25 +0200

    t

---
+++ paul/ai.py
@@ -0,0 +1,17 @@
+from thread import Thread
+
+class AI(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+        # Initially vision state is unknown
+        self.vision_state = {}
+
+    def on_message(self, msg):
+        # Received message, overwrite vision state
+        self.vision_state = msg
+
+    def on_tick(self):
+        # Do nothing if vision state is not known
+        print(2)
+
--- paul/main.py
+++ paul/main.py
@@ -1,7 +1,12 @@
 from vision import Vision
+from ai import AI
+
 
 vision = Vision()
+ai = AI()
 
 vision.start()
+ai.start()
 vision.join()
+ai.join()
 
