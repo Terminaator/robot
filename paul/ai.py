2dca593437080e46b62e1dd7899354e39737d718
Liibert Paul <oliibert@hotmail.com>
2019-11-18 15:35:52 +0200

    t

--- paul/ai.py
+++ paul/ai.py
@@ -9,12 +9,19 @@ class AI(Thread):
 
     def on_message(self, msg):
         # Received message, overwrite vision state
-        print(msg)
         self.vision_state = msg
 
     def on_tick(self):
         # Do nothing if vision state is not known
-        pass
+        if "closest_ball_coordinates" not in self.vision_state:
+            return
+
+        # Process vision state and decide action
+        # then send motor speeds command to mainboard
+        if self.vision_state["closest_ball_coordinates"][0] < 0:
+            print(self.vision_state)
+        else:
+            print(self.vision_state)
 
 
 ai = AI()
