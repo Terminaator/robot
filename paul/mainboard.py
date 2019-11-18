4876254224ad5b1e3b7abe9473012c429bcac674
Liibert Paul <oliibert@hotmail.com>
2019-11-18 17:45:15 +0200

    t

--- paul/mainboard.py
+++ paul/mainboard.py
@@ -37,9 +37,8 @@ class Mainboard(Thread):
         elif self.last_command == 'left':
             self.ser.write("sd:-10:-10:-10\n".encode())
         elif self.last_command == 'right':
-            self.ser.write("sd:0:-10:10\n".encode())
-            time.sleep(1. / 60)
             self.ser.write("sd:10:10:10\n".encode())
+            self.ser.write("sd:0:-10:10\n".encode())
         time.sleep(1./60)
         self.done = False
 
