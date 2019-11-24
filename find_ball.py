641de4144c3bead940d7c1f178df47277d8dd2d0
Liibert Paul <oliibert@hotmail.com>
2019-11-24 20:16:51 +0200

    e

--- find_ball.py
+++ find_ball.py
@@ -46,6 +46,8 @@ def basket_mask(frame):
                          np.array([cv2.getTrackbarPos("4", "Trackbars"), cv2.getTrackbarPos("5", "Trackbars"),
                                    cv2.getTrackbarPos("6", "Trackbars")]))
     #mask = cv2.dilate(mask, np.ones((3, 3), np.uint8), iterations=2)
+    closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((3, 3), np.uint8))
+
     return mask
 
 def find_blob(blob):  # returns the red colored circle
