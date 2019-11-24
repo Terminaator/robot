2a511a03d7b701413e8c4553fe10c95561041160
Liibert Paul <oliibert@hotmail.com>
2019-11-24 14:18:59 +0200

    t

--- test.py
+++ test.py
@@ -4,13 +4,9 @@ import serial.tools.list_ports
 
 ports = serial.tools.list_ports.comports()
 device = list(map(lambda port: port.device, ports))[0]
-
-ser = serial.Serial("COM7", 115200, timeout=0.01)
-
+ser = serial.Serial(device, 115200)
 
 while True:
-    ser.write("d:1500\n".encode())
-    time.sleep(1)
-    ser.write("d:3000\n".encode())
+    ser.write("sd:0:-10:10\n".encode())
     time.sleep(1)
 
