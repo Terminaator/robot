6072d29e55e0e91fad41fbacf706d0f0a3c8c0cc
Liibert Paul <oliibert@hotmail.com>
2019-11-24 18:32:20 +0200

    e

--- paul/ai.py
+++ paul/ai.py
@@ -47,6 +47,7 @@ class AI(Thread):
                     mainboard.second_wheel_speed(0)
                     mainboard.third_wheel_speed(0)
                     mainboard.send_message("fwd stop")
+                    self.isball = False
                 elif 250 < x < 390:
                     mainboard.first_wheel_speed(-40)
                     mainboard.second_wheel_speed(0)
