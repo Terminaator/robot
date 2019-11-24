3ad57e6ba0b7618eaf47cb790978926b3bdd9646
Liibert Paul <oliibert@hotmail.com>
2019-11-24 14:56:57 +0200

    t

--- paul/mainboard.py
+++ paul/mainboard.py
@@ -36,9 +36,9 @@ class Mainboard(Thread):
             self.ser.write("sd:10:0:-10\n".encode())
         elif self.last_command == 'stop':
             self.ser.write("sd:0:0:0\n".encode())
-        elif self.last_command == 'right':
-            self.ser.write("sd:-5:-5:-5\n".encode())
         elif self.last_command == 'left':
+            self.ser.write("sd:-5:-5:-5\n".encode())
+        elif self.last_command == 'right':
             self.ser.write("sd:5:5:5\n".encode())
         self.done = False
 
