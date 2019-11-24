6f5b7b506cb2021af474fa037d2152a8507356b3
Liibert Paul <oliibert@hotmail.com>
2019-11-24 14:53:37 +0200

    t

--- paul/ai.py
+++ paul/ai.py
@@ -26,25 +26,9 @@ class AI(Thread):
         x = self.vision_state["closest_ball_coordinates"][0]
         y = self.vision_state["closest_ball_coordinates"][1]
         distance = self.vision_state["distance"]
-        if x == 0 and y == 0:
-            mainboard.send_message("left")
-            self.last = "left"
-        else:
-            mainboard.send_message("stop")
+        mainboard.send_message("up")
+        self.last = "left"
 
-            if self.last == "left":
-                mainboard.send_message("stop")
-                self.last = None
-            else:
-                if x < 240:
-                    self.last = "right"
-                    mainboard.send_message("right")
-                elif x > 360:
-                    self.last = "left"
-                    mainboard.send_message("left")
-                elif 240 < x < 360:
-                    self.last = "up"
-                    mainboard.send_message("up")
         print(self.last)
 
 
