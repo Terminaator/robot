d0314a81a18ded6305dfdaa21e882129e8ab34d3
Liibert Paul <oliibert@hotmail.com>
2019-11-25 16:28:24 +0200

    ee

--- paul/ai.py
+++ paul/ai.py
@@ -1,8 +1,10 @@
 import time
 
+from simple_pid import PID
 from thread import Thread
 from mainboard import mainboard
 
+pid = PID(0.8, 0, 0.00001, setpoint=330)
 
 class AI(Thread):
     def __init__(self):
@@ -28,43 +30,11 @@ class AI(Thread):
         y_basket = self.vision_state["basket_coordinates"][1]
         basket_distance = self.vision_state["basket_distance"]
 
-        if x_ball == 0 and y_ball == 0:
-            if 3.5 < basket_distance < 20:
-                mainboard.first_wheel_speed(-40)
-                mainboard.second_wheel_speed(0)
-                mainboard.third_wheel_speed(40)
-                self.last = "NO_BALL_BASKET_GO"
-            else:
-                mainboard.first_wheel_speed(10)
-                mainboard.second_wheel_speed(10)
-                mainboard.third_wheel_speed(10)
-                self.last = "NO_BALL"
-        else:
-            if self.last in ["NO_BALL_BASKET_GO", "NO_BALL"]:
-                mainboard.first_wheel_speed(0)
-                mainboard.second_wheel_speed(0)
-                mainboard.third_wheel_speed(0)
-                self.last = "BALL_FOUND"
-            elif 250 < x_ball < 390:
-                mainboard.first_wheel_speed(0)
-                mainboard.second_wheel_speed(0)
-                mainboard.third_wheel_speed(0)
-                self.last = "FWD"
-            elif x_ball < 250:
-                speed = (250 - x_ball) / 28.9
-                mainboard.first_wheel_speed(int(-speed))
-                mainboard.second_wheel_speed(int(-speed))
-                mainboard.third_wheel_speed(int(-speed))
-                print(speed)
-                self.last = "LEFT_BALL"
-            elif x_ball > 390:
-                speed = (x_ball - 390) / 28.9
-                mainboard.first_wheel_speed(int(-speed))
-                mainboard.second_wheel_speed(int(-speed))
-                mainboard.third_wheel_speed(int(-speed))
-                print(speed)
-                self.last = "RIGHT_BALL"
-        mainboard.send_message(self.last)
+        wheelspeed = int(pid(x_ball))
+        print(wheelspeed)
+        wheelspeed1 = int(pid(y_ball))
+        print(wheelspeed1)
+
 
 
 ai = AI()
