4e117de3c9dcfab82620ce9838065938788b64ea
Liibert Paul <oliibert@hotmail.com>
2019-11-25 14:49:55 +0200

    ee

--- paul/ai.py
+++ paul/ai.py
@@ -17,72 +17,31 @@ class AI(Thread):
         self.vision_state = msg
 
     def on_tick(self):
-        # Do nothing if vision state is not known
         if "ball_coordinates" not in self.vision_state:
             return
-        x = self.vision_state["ball_coordinates"][0]
-        y = self.vision_state["ball_coordinates"][1]
-        distance = self.vision_state["ball_distance"]
 
-        if x == 0 and y == 0:
-            if 5 < self.vision_state["basket_distance"] < 9:
+        x_ball = self.vision_state["ball_coordinates"][0]
+        y_ball = self.vision_state["ball_coordinates"][1]
+        ball_distance = self.vision_state["ball_distance"]
+
+        x_basket = self.vision_state["basket_coordinates"][0]
+        y_basket = self.vision_state["basket_coordinates"][1]
+        basket_distance = self.vision_state["basket_distance"]
+
+        if x_ball == 0 and y_ball == 0:
+            if 5 < basket_distance < 20:
                 mainboard.first_wheel_speed(-40)
                 mainboard.second_wheel_speed(0)
                 mainboard.third_wheel_speed(40)
+                self.last = "NO_BALL_BASKET_GO"
             else:
-                mainboard.first_wheel_speed(5)
-                mainboard.second_wheel_speed(5)
-                mainboard.third_wheel_speed(5)
-                mainboard.send_message("turn")
+                mainboard.first_wheel_speed(10)
+                mainboard.second_wheel_speed(10)
+                mainboard.third_wheel_speed(10)
                 self.last = "NO_BALL"
         else:
-            if self.last == "NO_BALL":
-                mainboard.first_wheel_speed(0)
-                mainboard.second_wheel_speed(0)
-                mainboard.third_wheel_speed(0)
-                mainboard.send_message("stop")
-                self.last = None
-            elif 250 < x < 390 and distance < 0.3:
-                if self.last == "fwd stop":
-                    x = self.vision_state["basket_coordinates"][0]
-                    y = self.vision_state["basket_coordinates"][1]
-                    if 250 < x < 390:
-                        mainboard.first_wheel_speed(0)
-                        mainboard.second_wheel_speed(0)
-                        mainboard.third_wheel_speed(0)
-                        self.last = "stop"
-                    elif x < 250:
-                        mainboard.first_wheel_speed(0)
-                        mainboard.second_wheel_speed(-20)
-                        mainboard.third_wheel_speed(0)
-                    elif x > 390:
-                        mainboard.first_wheel_speed(0)
-                        mainboard.second_wheel_speed(-20)
-                        mainboard.third_wheel_speed(0)
-                else:
-                    mainboard.first_wheel_speed(0)
-                    mainboard.second_wheel_speed(0)
-                    mainboard.third_wheel_speed(0)
-                    mainboard.send_message("fwd stop")
-                    self.last = "fwd stop"
-
-            elif 250 < x < 390:
-                mainboard.first_wheel_speed(-40)
-                mainboard.second_wheel_speed(0)
-                mainboard.third_wheel_speed(40)
-                mainboard.send_message("fwd")
-            elif x < 250:
-                mainboard.first_wheel_speed(-20)
-                mainboard.second_wheel_speed(-20)
-                mainboard.third_wheel_speed(-20)
-                mainboard.send_message("left")
-            elif x > 390:
-                mainboard.first_wheel_speed(20)
-                mainboard.second_wheel_speed(20)
-                mainboard.third_wheel_speed(20)
-                mainboard.send_message("right")
-            else:
-                mainboard.send_message(None)
+            pass
+        mainboard.send_message("command")
 
 
 ai = AI()
