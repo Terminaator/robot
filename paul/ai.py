0e28cc8648359190fc8f325a2c92d95462a27daa
Liibert Paul <oliibert@hotmail.com>
2019-11-25 19:04:14 +0200

    ee

--- paul/ai.py
+++ paul/ai.py
@@ -20,7 +20,7 @@ class AI(Thread):
         self.vision_state = msg
 
     def angle(self, ball_x, ball_y):
-        return math.degrees(atan2(ball_y-240,320-ball_x))
+        return math.degrees(atan2(240-ball_y,320-ball_x))
 
     def on_tick(self):
         if "ball_coordinates" not in self.vision_state:
