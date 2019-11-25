c013142b1d6cf15f5aeca319674751f62a3e9297
Liibert Paul <oliibert@hotmail.com>
2019-11-25 20:04:20 +0200

    ewe

--- paul/ai.py
+++ paul/ai.py
@@ -39,19 +39,19 @@ class AI(Thread):
         wheelLinearVelocity2 = -30 * cos(math.radians(robotDirectionAngle - 120 + 90))
         wheelLinearVelocity3 = -30 * cos(math.radians(robotDirectionAngle - 240 + 90))
         if 250 < x_ball < 390 and y_ball > 350:
-            if 270 < x_basket < 370:
+            if 280 < x_basket < 360:
                 mainboard.first_wheel_speed(-40)
                 mainboard.second_wheel_speed(0)
                 mainboard.third_wheel_speed(40)
                 mainboard.throw(1500)
                 self.last = "FWD_STOP_FIRST"
-            elif x_basket < 270:
+            elif x_basket < 280:
                 mainboard.first_wheel_speed(0)
                 mainboard.second_wheel_speed(-20)
                 mainboard.third_wheel_speed(0)
                 self.last = "FWD_STOP"
 
-            elif x_basket > 370:
+            elif x_basket > 360:
                 mainboard.first_wheel_speed(0)
                 mainboard.second_wheel_speed(-20)
                 mainboard.third_wheel_speed(0)
