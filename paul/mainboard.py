a5b84eec7eeb02b47ec8f7c82be145baa136ee02
Liibert Paul <oliibert@hotmail.com>
2019-11-27 19:33:35 +0200

    omni

--- paul/mainboard.py
+++ paul/mainboard.py
@@ -1,3 +1,4 @@
+import math
 import serial.tools.list_ports
 from thread import Thread
 
@@ -9,10 +10,24 @@ class Mainboard(Thread):
         device = list(map(lambda port: port.device, ports))[0]
 
         self.ser = serial.Serial(device, 115200)
+        self.speed_one = 0
+        self.speed_two = 0
+        self.speed_three = 0
+
         self.last_command = None
 
     def on_message(self, msg):
         self.last_command = msg
+
+    def angle(self, ball_x, ball_y):
+        return math.degrees(math.atan2(320 - ball_x, 480 - ball_y))
+
+    def omni_monition(self, x_ball, y_ball):
+        robotDirectionAngle = self.angle(x_ball, y_ball)
+
+        self.speed_two = -40 * math.cos(math.radians(robotDirectionAngle - 0 + 90))
+        self.speed_one = -40 * math.cos(math.radians(robotDirectionAngle - 120 + 90))
+        self.speed_three = -40 * math.cos(math.radians(robotDirectionAngle - 240 + 90))
 
     def on_tick(self):
         while self.ser.in_waiting:
@@ -20,5 +35,6 @@ class Mainboard(Thread):
         if self.last_command is None:
             return
         print(self.last_command)
+
 
 mainboard = Mainboard()
