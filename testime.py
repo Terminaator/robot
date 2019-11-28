f51378dd3f90fd07e36c96399453a26f264187b8
Liibert Paul <oliibert@hotmail.com>
2019-11-28 20:49:26 +0200

    go forward

---
+++ testime.py
@@ -0,0 +1,10 @@
+import serial.tools.list_ports
+
+
+ports = serial.tools.list_ports.comports()
+device = list(map(lambda port: port.device, ports))[0]
+
+ser = serial.Serial("COM7", 115200)
+
+while True:
+    ser.write("sd:10:10:10\n".encode())
