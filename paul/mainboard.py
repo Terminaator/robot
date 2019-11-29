2a4c3d0d3c8c9684404a4eee93443ef2c462dae2
Liibert Paul <paul.liibert@tieto.com>
2019-11-29 20:48:22 +0200

    kaamera

--- paul/mainboard.py
+++ paul/mainboard.py
@@ -38,7 +38,7 @@ class Mainboard(Thread):
     def set_speeds(self):
         if self.last_command == "THROW_BALL":
             self.go_forward = 15
-            self.set_speeds_wheels(-40, 0, 40)
+            self.set_speeds_wheels(-20, 0, 20)
         elif self.last_command == "NO_BALL_BASKET_GO":
             self.set_speeds_wheels(-40, 0, 40)
         elif self.last_command == "NO_BALL":
