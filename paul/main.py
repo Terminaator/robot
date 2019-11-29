88a508ac0ef2ecd14020fda1cec52f2f78f25c67
Liibert Paul <paul.liibert@tieto.com>
2019-11-29 19:52:59 +0200

    kaamera

--- paul/main.py
+++ paul/main.py
@@ -1,14 +1,32 @@
 from ai import ai
+from pynput import keyboard
 from vision import vision
 from mainboard import mainboard
-        
-ai.start()
-vision.start()
-mainboard.start()
+
+
+def on_press(key):
+    try:
+        k = key.char  # single-char keys
+    except:
+        k = key.name  # other keys
+    global throw
+    if k == 'enter':
+        print("OK")
+    print(k)
+
+
+
+lis = keyboard.Listener(on_press=on_press)
+lis.start()
+
+lis.join()
+#ai.start()
+#vision.start()
+#mainboard.start()
 
 
 
 
-ai.join()
-vision.join()
-mainboard.join()
+#ai.join()
+#vision.join()
+#mainboard.join()
