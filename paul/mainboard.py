8f558c3718ec05fdb6efffd8345dd4a854f291ba
Liibert Paul <paul.liibert@tieto.com>
2019-11-30 00:32:31 +0200

    kaamera

--- paul/mainboard.py
+++ paul/mainboard.py
@@ -71,8 +71,8 @@ class Mainboard(Thread):
 
         if self.go_forward > 0:
             self.last_command = "OMNIDIRECTIONAL_THROW"
-            command = "sd:" + str(self.speed_one) + ":" + str(self.speed_two) + ":" + str(self.speed_three) + "\n"
-            command += "d:" + str(self.thrower_speed) + "\n"
+            command = "sd:" + str(self.speed_one) + ":" + str(self.speed_two) + ":" + str(self.speed_three) + "\n" + \
+                      "d:" + str(self.thrower_speed) + "\n"
             self.go_forward -= 1
         else:
             self.set_speeds()
