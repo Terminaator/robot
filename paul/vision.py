3565d7e2201ed52397e4f511bbc77a235b0b88e8
Liibert Paul <paul.liibert@tieto.com>
2019-11-29 21:09:28 +0200

    kaamera

--- paul/vision.py
+++ paul/vision.py
@@ -109,7 +109,7 @@ class Vision(Thread):
             "basket_distance": (depth_frame.get_distance(int(x_basket), int(y_basket)))
         })
 
-        cv2.imshow('frame', basket_mask)
+        cv2.imshow('frame', self.frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             return
 
