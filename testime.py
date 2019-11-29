1332ecaa24a49d6cf1a920e0897fc58a3ba6482b
Liibert Paul <paul.liibert@tieto.com>
2019-11-29 22:32:55 +0200

    kaamera

--- testime.py
+++ testime.py
@@ -1,11 +1,33 @@
 import serial.tools.list_ports
-import  time
+import time
+import threading
 
 ports = serial.tools.list_ports.comports()
 device = list(map(lambda port: port.device, ports))[0]
 
 ser = serial.Serial(device, 115200)
 
-while True:
-    ser.write("sd:10:10:10\n".encode())
-    time.sleep(1)
+
+# define a thread which takes input
+class InputThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        while True:
+            speed = input('input something: ')
+            i = 3
+            while i > 0:
+                i -= 1
+                ser.write(("d:" + str(speed) + "\n").encode())
+                time.sleep(0.2)
+            # do something based on the user input here
+            # alternatively, let main do something with
+            # self.last_user_input
+
+
+# main
+it = InputThread()
+it.setDaemon(True)  # first
+it.start()  # second
+it.join()
