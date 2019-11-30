4446e8c5f19642a8c1e416e706ee4937897369f6
Liibert Paul <paul.liibert@tieto.com>
2019-11-30 15:06:25 +0200

    turn

--- paul/vision.py
+++ paul/vision.py
@@ -111,7 +111,7 @@ class Vision(Thread):
             "basket_distance": (depth_frame.get_distance(int(x_basket), int(y_basket)))
         })
 
-        cv2.imshow('frame', self.ball_mask)
+        cv2.imshow('frame', ball_mask)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             return
 
